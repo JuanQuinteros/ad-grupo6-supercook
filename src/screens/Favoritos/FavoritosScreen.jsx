e22864529d6d2d0b05f09e3167cce685540ead5d
Lau <marialaura.severiens@gmail.com>
2022-07-04 21:59:30 -0300

    recetas_favoritas

--- src/screens/Favoritos/FavoritosScreen.jsx
+++ src/screens/Favoritos/FavoritosScreen.jsx
@@ -1,18 +1,60 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { View, ScrollView } from 'react-native';
+import { Text, Title } from 'react-native-paper';
+import { useQuery } from 'react-query';
+import HomeLayout from '../../layouts/HomeLayout';
+import * as userApi from '../../api/user';
+import RecipeCard from '../../components/RecipeCard';
+import * as favoritesApi from '../../api/favorites';
 
-function FavoritosScreen() {
+function FavoritosScreen({ navigation }) {
+  const { data: usuario } = useQuery('user', userApi.getUser, {
+    placeholderData: { nombre: 'Invitado' },
+    select: (data) => data.usuario,
+  });
+
+  const { data: favoritos } = useQuery('favorites', favoritesApi.favoritos, {
+    placeholderData: [],
+  });
+
+  function handleIconPress() {
+    navigation.navigate('Perfil');
+  }
+
+  function handleRecipePress(recipe) {
+    const { user, ...receta } = recipe;
+    navigation.navigate('Receta', { recetaId: recipe.id })
+  }
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{fontSize: 30}}>
-        En construcción
-      </Text>
-      <Text style={{fontSize: 30}}>
-        🚧
-      </Text>
-    </View>
-  )
+    // <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    //   <Text style={{fontSize: 30}}>
+    //     En construcción
+    //   </Text>
+    //   <Text style={{fontSize: 30}}>
+    //     🚧
+    //   </Text>
+    // </View>
+
+    <HomeLayout
+      icon="account-circle-outline"
+      title={`Hola ${usuario.nombre}`}
+      onIconPress={handleIconPress}
+      padding={16}
+    >
+      <Title>Recetas Favoritas</Title>
+      {/* {console.log(JSON.stringify(favoritos))} */}
+      <ScrollView style={{marginTop: 10}}>
+        {favoritos.map(r => (
+          <RecipeCard
+            key={r.id}
+            recipe={r}
+            onPress={handleRecipePress}
+          />
+        ))}
+      </ScrollView>
+    </HomeLayout>
+  );
 }
 
 export default FavoritosScreen;
