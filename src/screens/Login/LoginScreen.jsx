377b31a840e6a83188c58a047f762b56769e8a17
JuanQuinteros <juan.quinteros.dev@gmail.com>
2022-07-07 12:40:53 -0300

    Acomodar la imagen del login
    Está un poco mejor nada más...

--- src/screens/Login/LoginScreen.jsx
+++ src/screens/Login/LoginScreen.jsx
@@ -16,8 +16,7 @@ import { useMutation } from 'react-query';
 import axios from 'axios';
 import { surface, superCook } from '../../styles/colors';
 import * as loginApi from '../../api/login';
-
-
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const reviewSchema = yup.object({
   email: yup.string().email().required(),
@@ -69,95 +68,96 @@ export default function LoginScreen({ navigation }) {
   }
 
   return (
-    <ScrollView style={styles.container} contentContainerStyle={{ flexGrow: 1 }}>
-      <View style={styles.formView}>
-        <Text style={{ fontSize: 55, marginBottom: 5, fontFamily: 'WendyOne' }}>
-          SuperCook
-        </Text>
-        {/* <Image source={require('../../../assets/supercook.png')} /> */}
-        <Image source={superCook} style={{width: '25%',  height: '25%'}} />
-        <Formik
-          initialValues={initialValues}
-          validationSchema={reviewSchema}
-          onSubmit={handleFormikSubmit}
-        >
-          {({
-            handleChange, handleBlur, handleSubmit, isValid, errors, touched, values,
-          }) => (
-            <>
-              <TextInput
-                mode="flat"
-                label="Email"
-                style={styles.textInput}
-                textContentType="emailAddress"
-                keyboardType="email-address"
-                onSubmitEditing={() => passwordTextInput.current.focus()}
-                blurOnSubmit={false}
-                returnKeyType="next"
-                onBlur={handleBlur('email')}
-                error={touched.email && errors.email}
-                value={values.email}
-                onChangeText={handleChange('email')}
-              />
-              <TextInput
-                mode="flat"
-                label="Contraseña"
-                style={styles.textInput}
-                secureTextEntry
-                textContentType="password"
-                ref={passwordTextInput}
-                onSubmitEditing={handleSubmit}
-                onBlur={handleBlur('password')}
-                error={touched.password && errors.password}
-                value={values.password}
-                onChangeText={handleChange('password')}
-              />
-              {/* Checkbox */}
-              <View style={styles.checkbox}>
-                <Checkbox
-                  status={recordarme ? 'checked' : 'unchecked'}
-                  onPress={onRecordarmeCheckboxClick}
+    <SafeAreaView style={{flex: 1, flexGrow: 1}}>
+      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+        <View style={styles.formView}>
+          <Text style={{ fontSize: 55, marginBottom: 5, fontFamily: 'WendyOne' }}>
+            SuperCook
+          </Text>
+          <Image source={superCook} style={{width: '25%',  height: '25%'}} />
+          <Formik
+            initialValues={initialValues}
+            validationSchema={reviewSchema}
+            onSubmit={handleFormikSubmit}
+          >
+            {({
+              handleChange, handleBlur, handleSubmit, isValid, errors, touched, values,
+            }) => (
+              <>
+                <TextInput
+                  mode="flat"
+                  label="Email"
+                  style={styles.textInput}
+                  textContentType="emailAddress"
+                  keyboardType="email-address"
+                  onSubmitEditing={() => passwordTextInput.current.focus()}
+                  blurOnSubmit={false}
+                  returnKeyType="next"
+                  onBlur={handleBlur('email')}
+                  error={touched.email && errors.email}
+                  value={values.email}
+                  onChangeText={handleChange('email')}
                 />
-                <Text style={{ fontWeight: 'bold' }}>Recordarme</Text>
-              </View>
-              <View style={{ flexDirection: 'column', alignSelf: 'stretch' }}>
-                <Button
-                  mode="contained"
-                  onPress={handleSubmit}
-                  style={{ marginTop: 20, alignSelf: 'stretch' }}
-                  disabled={!isValid || isLoading}
-                  loading={isLoading}
-                >
-                  Login
-                </Button>
-              </View>
-            </>
-          )}
-        </Formik>
-        <View style={styles.registerView}>
-          <Button
-            mode="text"
-            onPress={onRecuperoButtonClick}
-            style={{ marginTop: 20, alignSelf: 'flex-end' }}
-            uppercase={false}
-            color={colors.text}
-            compact
-          >
-            Olvidaste tu contraseña?
-          </Button>
-          <Button
-            mode="text"
-            onPress={onRegistrateButtonClick}
-            uppercase={false}
-            color={colors.text}
-            compact
-          >
-            No tenés una cuenta? Registrate
-          </Button>
+                <TextInput
+                  mode="flat"
+                  label="Contraseña"
+                  style={styles.textInput}
+                  secureTextEntry
+                  textContentType="password"
+                  ref={passwordTextInput}
+                  onSubmitEditing={handleSubmit}
+                  onBlur={handleBlur('password')}
+                  error={touched.password && errors.password}
+                  value={values.password}
+                  onChangeText={handleChange('password')}
+                />
+                {/* Checkbox */}
+                <View style={styles.checkbox}>
+                  <Checkbox
+                    status={recordarme ? 'checked' : 'unchecked'}
+                    onPress={onRecordarmeCheckboxClick}
+                  />
+                  <Text style={{ fontWeight: 'bold' }}>Recordarme</Text>
+                </View>
+                <View style={{ flexDirection: 'column', alignSelf: 'stretch' }}>
+                  <Button
+                    mode="contained"
+                    onPress={handleSubmit}
+                    style={{ marginTop: 20, alignSelf: 'stretch' }}
+                    disabled={!isValid || isLoading}
+                    loading={isLoading}
+                  >
+                    Login
+                  </Button>
+                </View>
+              </>
+            )}
+          </Formik>
+          <View style={styles.registerView}>
+            <Button
+              mode="text"
+              onPress={onRecuperoButtonClick}
+              style={{ marginTop: 20, alignSelf: 'flex-end' }}
+              uppercase={false}
+              color={colors.text}
+              compact
+            >
+              Olvidaste tu contraseña?
+            </Button>
+            <Button
+              mode="text"
+              onPress={onRegistrateButtonClick}
+              uppercase={false}
+              color={colors.text}
+              compact
+            >
+              No tenés una cuenta? Registrate
+            </Button>
+          </View>
         </View>
-      </View>
+      </ScrollView>
       <StatusBar />
-    </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -167,7 +167,6 @@ const styles = StyleSheet.create({
     surface,
   },
   formView: {
-    flex: 1,
     padding: 32,
     flexDirection: 'column',
     alignItems: 'center',
